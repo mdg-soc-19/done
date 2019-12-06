443f30b610ad9234ed4de27a50e1fe14e1a05624
Aarushi VIj <aarushivij@gmail.com>
2019-12-07 02:01:53 +0530

    fragments made

--- app/src/main/java/com/example/android/done/MainActivity.java
+++ app/src/main/java/com/example/android/done/MainActivity.java
@@ -15,18 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // Find the View that shows the new_goal category
-        TextView numbers = (TextView) findViewById(R.id.new_goal);
-
-// Set a click listener on that View
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, GoalName.class);
-                startActivity(numbersIntent);
-            }
-        });
 
     }
 
