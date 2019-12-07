f475a0ae8a157c52b85b044e71111d8489e97ad6
Aarushi VIj <aarushivij@gmail.com>
2019-12-07 23:44:41 +0530

    fragment navigation

---
+++ app/src/main/java/com/example/android/done/HomePage.java
@@ -0,0 +1,46 @@
+package com.example.android.done;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class HomePage extends Fragment {
+    NavController navController;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        TextView newGoal = (TextView) view.findViewById(R.id.new_goal);
+        newGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_HomePage_to_goalName);
+            }
+        });
+
+
+    }
+}
