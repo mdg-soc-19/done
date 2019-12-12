1d68e9a9c5e9a1a8b0ce4e57806aa18f30250d1b
Aarushi VIj <aarushivij@gmail.com>
2019-12-12 17:57:56 +0530

    make MyGoalsHome fragment navigation

---
+++ app/src/main/java/com/example/android/done/Goal.java
@@ -0,0 +1,19 @@
+package com.example.android.done;
+
+import androidx.room.Entity;
+
+@Entity
+public class Goal {
+
+    private int id;
+
+    private String goalName;
+
+    private String motivation;
+
+    private String priority;
+
+    private Long deadline;
+
+    
+}
--- app/src/main/java/com/example/android/done/SetReminders.java
+++ app/src/main/java/com/example/android/done/SetReminders.java
@@ -4,18 +4,27 @@ package com.example.android.done;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
 import java.util.Calendar;
 
 
 public class SetReminders extends Fragment {
 
+    NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,30 +32,21 @@ public class SetReminders extends Fragment {
 
 
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_set_reminders, container, false);
-        final Button before_button = (Button) v.findViewById(R.id.before_button);
-        before_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
+        return inflater.inflate(R.layout.fragment_set_reminders, container, false);
 
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        before_button.setText( selectedHour + ":" + selectedMinute);
-                    }
-                }, hour, minute, false);
-                mTimePicker.setTitle("Select Time");
-                mTimePicker.show();
-            }
-        });
-
-
-return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        Button save = (Button) view.findViewById(R.id.save_button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                    navController.navigate(R.id.action_SetReminders_to_MyGoalsHome);
+            }
+        });
+    }
 }
