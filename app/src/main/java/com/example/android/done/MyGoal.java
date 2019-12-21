ac2f6e25884b1cab2b3d5f50abfb9f6bf133ef71
aarushivij <aarushivij@gmail.com>
2019-12-21 22:22:25 +0530

    make goal layout page

--- app/src/main/java/com/example/android/done/MyGoal.java
+++ app/src/main/java/com/example/android/done/MyGoal.java
@@ -28,31 +28,8 @@ public class MyGoal extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_my_goal, container, false);
-        setHasOptionsMenu(true);
-        return v;
+        return inflater.inflate(R.layout.fragment_my_goal, container, false);
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.my_goal_toolbar , menu);
-        super.onCreateOptionsMenu(menu , inflater);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.edit_action:
-                Toast.makeText(getContext(), "edit", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.calendar_action:
-                Toast.makeText(getContext(), "calendar", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.stats_action:
-                Toast.makeText(getContext(), "STATISTICS", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
 }
