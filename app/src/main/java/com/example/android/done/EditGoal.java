97f4a82702d781033383fa4e7f71af489709c1c9
aarushivij <aarushivij@gmail.com>
2019-12-21 23:50:32 +0530

    edit goal layout made

---
+++ app/src/main/java/com/example/android/done/EditGoal.java
@@ -0,0 +1,43 @@
+package com.example.android.done;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditGoal extends Fragment {
+
+
+    public EditGoal() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_edit_goal, container, false);
+
+        String[] values =
+                {"A", "B", "C", "D"};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        return v;
+
+    }
+
+}
